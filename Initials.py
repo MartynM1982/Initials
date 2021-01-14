0dc7dde47f5db0ea1965f4d8ab842c1b595f807d
Martyn Minker <77343504+MartynM1982@users.noreply.github.com>
2021-01-14 13:20:10 +0000

    Add files via upload

---
+++ Initials.py
@@ -0,0 +1,9 @@
+# My initials are double M, as my name is Martyn Minker.
+print("M   M  M   M")
+print("MM MM  MM MM")
+print("MM MM  MM MM")
+print("M M M  M M M")
+print("M   M  M   M")
+print("M   M  M   M")
+print("M   M  M   M")
+print("M   M  M   M")
